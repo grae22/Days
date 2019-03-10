d5226984e48d3bf6716538291ca218bb606b7565
GraemeB <grae22@gmail.com>
2019-03-10 20:43:44 +0200

    Script with some initial basic menu nav.

---
+++ days.ps1
@@ -0,0 +1,45 @@
+﻿# Functions ===================================================================
+
+function RenderMainMenu()
+{
+    Write-Host "--- Days ---`n"
+    Write-Host "1. Add entry"
+    Write-Host "0. Exit`n"
+}
+
+function DoMainMenu()
+{
+    do
+    {
+        Clear-Host
+        RenderMainMenu
+
+        $input = Read-Host -Prompt 'Choose an option'
+
+        switch ($input)
+        {
+            '1'
+            {
+                DoAddEntry
+            }
+
+            '0'
+            {
+                Exit
+            }
+        }
+    }
+    while ($true)
+}
+
+function DoAddEntry()
+{
+    Clear-Host
+    Write-Host "--- Add Entry ---`n"
+    
+    $name = Read-Host -Prompt 'Name'
+}
+
+# Logic =======================================================================
+
+DoMainMenu
