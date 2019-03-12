75a17aac694ba79622ece3ad2704adfa4388cadf
GraemeB <graeme.bruschi@derivco.co.za>
2019-03-12 07:13:50 +0200

    New entry input details done.

--- days.ps1
+++ days.ps1
@@ -25,7 +25,7 @@ function DoMainMenu()
 
             '0'
             {
-                Exit
+                return
             }
         }
     }
@@ -37,9 +37,75 @@ function DoAddEntry()
     Clear-Host
     Write-Host "--- Add Entry ---`n"
     
-    $name = Read-Host -Prompt 'Name'
+    $name = GetTrimmedInput 'Name'
+
+    if ($name.Length -eq 0)
+    {
+        return
+    }
+    
+    $rating = GetInputNumberInRange 'Rating (1=V.Bad, 2=Bad, 3=Poor, 4=Avg, 5=Ok, 6=Good, 7=V.Good, 0=Cancel)' (0..7)
+
+    if ($rating -eq 0)
+    {
+        return
+    }
+
+    $date = GetInputDate 'Date'
+
+    Write-Host "'nName: $name Rating: $rating Date: $date"
+    Read-host -Prompt "'nContinue"
+}
+
+function GetNonEmptyInput([string]$prompt)
+{
+    do
+    {
+        $input = Read-Host -Prompt $prompt
+
+        if ($input.Length -gt 0)
+        {
+            return $input
+        }
+    }
+    while ($true)
+}
+
+function GetTrimmedInput([string]$prompt)
+{
+    return (Read-Host -Prompt $prompt).Trim()
+}
+
+function GetInputNumberInRange([string]$prompt, [int[]]$range)
+{
+    while ($true)
+    {
+        $input = GetTrimmedInput $prompt
+        $inputAsInt = 0
+        
+        if ([System.Int32]::TryParse($input, [ref] $inputAsInt) -and $range.Contains($inputAsInt))
+        {
+            return $inputAsInt
+        }
+    }    
+}
+
+function GetInputDate([string]$prompt)
+{
+    while ($true)
+    {
+        $input = Read-Host -Prompt $prompt
+        $inputAsDate = [System.DateTime]'2019-1-1'
+
+        if ([System.DateTime]::TryParse($input, [ref]$inputAsDate))
+        {
+            return $inputAsDate
+        }
+    }
 }
 
 # Logic =======================================================================
 
 DoMainMenu
+
+Write-Host "`nBye."
